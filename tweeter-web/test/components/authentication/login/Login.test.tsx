c7415a4cc9fffe2c47443d44c11fa07d05df9241
James Finlinson <the30clues@gmail.com>
2024-10-22 12:27:56 -0600

    Create Login Component tests

--- tweeter-web/test/components/authentication/login/Login.test.tsx
+++ tweeter-web/test/components/authentication/login/Login.test.tsx
@@ -16,13 +16,35 @@ describe('Login Component', () => {
     expect(signInButton).toBeDisabled();
   });
 
-  it('enables the sign-in button if both alias and password fields have text', async () => {
-    const {signInButton, aliasField, passwordField, user} = renderLoginAndGetElement("/");
+  async function enableSignInButton(elements: ReturnType<typeof renderLoginAndGetElement>) {
+    const {signInButton, aliasField, passwordField, user} = elements;
 
     await user.type(aliasField, "a");
     await user.type(passwordField, "b");
 
     expect(signInButton).toBeEnabled();
+  }
+
+  it('enables the sign-in button if both alias and password fields have text', async () => {
+    const elements = renderLoginAndGetElement("/");
+    await enableSignInButton(elements);
+  });
+
+  it('disables the sign-in button if either field is cleared', async () => {
+    const elements = renderLoginAndGetElement("/");
+    await enableSignInButton(elements);
+
+    const {signInButton, aliasField, passwordField, user} = elements;
+
+    // Try clearing alias field
+    await user.clear(aliasField);
+    expect(signInButton).toBeDisabled();
+
+    await enableSignInButton(elements);
+
+    // Try clearing password field
+    await user.clear(passwordField);
+    expect(signInButton).toBeDisabled();
   });
 });
 
