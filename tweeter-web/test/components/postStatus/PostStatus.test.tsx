956f3d077f496d978fae313095c52a38520b252b
James Finlinson <the30clues@gmail.com>
2024-10-22 12:56:48 -0600

    Stub out PostStatus Component tests

---
+++ tweeter-web/test/components/postStatus/PostStatus.test.tsx
@@ -0,0 +1,19 @@
+import "../../ui-utils";
+
+describe('PostStatus Component', () => {
+  it('begins with PostStatus and Clear buttons disabled', async () => {
+
+  });
+
+  it('enables both buttons when the text field has text', async () => {
+
+  });
+
+  it('disables both buttons when the text field is cleared', async () => {
+
+  });
+
+  it("calls the presenter's postStatus method with the correct parameters when PostStatus button is pressed", async () => {
+
+  });
+});
