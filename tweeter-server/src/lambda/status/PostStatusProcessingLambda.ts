f132ae3d592e5ea11610fabd75185da08bc1f489
James Finlinson <the30clues@gmail.com>
2024-12-10 12:29:01 -0700

    Fix string parsing for SQS service

--- tweeter-server/src/lambda/status/PostStatusProcessingLambda.ts
+++ tweeter-server/src/lambda/status/PostStatusProcessingLambda.ts
@@ -2,7 +2,9 @@ import { buildStatusService } from "../helper/factory";
 import { queueHandler } from "../helper/QueueHandler";
 import { PostStatusProcessingRequest } from "./PostStatusProcessingRequest";
 
-export const handler = queueHandler(async (request: PostStatusProcessingRequest): Promise<void> => {
+export const handler = queueHandler((requestStr: string): Promise<void> => {
   const service = buildStatusService();
+  // API Gateway does not serialize these items for us.
+  const request = JSON.parse(requestStr) as PostStatusProcessingRequest;
   return service.postStatusProcessing(request.feedItem, request.toFollowerAliases);
 });
--- tweeter-server/src/lambda/status/PostStatusSpawnLambda.ts
+++ tweeter-server/src/lambda/status/PostStatusSpawnLambda.ts
@@ -2,7 +2,9 @@ import { FeedStoryDTO } from "../../model/dto/FeedStoryDTO";
 import { buildStatusService } from "../helper/factory";
 import { queueHandler } from "../helper/QueueHandler";
 
-export const handler = queueHandler(async (feedItem: FeedStoryDTO): Promise<void> => {
+export const handler = queueHandler((feedItemStr: string): Promise<void> => {
   const service = buildStatusService();
-  await service.postStatusSpawn(feedItem);
+  // API Gateway doesn't parse this for us.
+  const feedItem = JSON.parse(feedItemStr) as FeedStoryDTO;
+  return service.postStatusSpawn(feedItem);
 });
