ec48923b54f3e2c3a4bfa80ae5ce0f15fb9499f2
James Finlinson <the30clues@gmail.com>
2024-12-11 09:20:44 -0700

    Add typing context and details to QueueHandler logs

--- tweeter-server/src/lambda/helper/QueueHandler.ts
+++ tweeter-server/src/lambda/helper/QueueHandler.ts
@@ -1,10 +1,10 @@
 /** A helper function that handles all items in a queue, calling {@linkcode onMessage} for each message. */
 export const queueHandler = function<T>(onMessage: (obj: T) => Promise<void>) {
   return async function(event: any) {
-    console.log("Invoking lambda handler")
+    console.log(`Invoking lambda handler (${typeof event}): ${JSON.stringify(event)}`);
     for (let i = 0; i < event.Records.length; ++i) {
       const { body } = event.Records[i];
-      console.log(`Received message: ${body}`);
+      console.log(`Received message (${typeof body}): ${body}`);
       onMessage(body as T);
     }
     return null;
